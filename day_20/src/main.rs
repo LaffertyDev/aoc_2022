05291bf98b38843ac86520cc7e3a34959be0a001
Nathan Lafferty <nslafferty@gmail.com>
2022-12-21 18:34:46 -0600

    day 20a correct

--- day_20/src/main.rs
+++ day_20/src/main.rs
@@ -23,107 +23,42 @@ fn mix_cipher(data: Vec<i32>) -> Vec<i32> {
     }
 
     for (key, amount_to_shift) in data.iter().enumerate() {
-        let bounded_amount_to_shift = amount_to_shift % (data.len() as i32);
+
+        println!("{:?}", cipher_mutation.iter().map(|(key, shift)| (key, shift)).collect::<Vec<(&usize, &i32)>>());
+        if amount_to_shift == &0 {
+            continue; // nothing to do
+        }
+
+
+        // A
+        // B C D E
+        // start index is 0
+        // 1 -> B; dest index should be 1 (insert before)
+        // 2 -> C; dest index should be 2 (C is at 1, D is at 2, so insert before D)
+        // 3 -> D
+        // 4 (0) -> E (insert before B at index 0)
 
         // first we find the node
         let index_of_node_needing_moved = cipher_mutation.iter().enumerate().find(|(_i, c)| c.0 == key).map(|(i, _c)| i).unwrap();
 
-        // a true modulo command would be useful here
-        let destination_index = if bounded_amount_to_shift.is_negative() && (bounded_amount_to_shift.abs() as usize) > index_of_node_needing_moved {
-            // we wrapped to the left
-            data.len() - ((bounded_amount_to_shift + index_of_node_needing_moved as i32).abs() as usize % (data.len()))
+        // then we remove the node we're interested in from the set, we'll re-add it later
+        let mut nodes = cipher_mutation.split_off(index_of_node_needing_moved);
+        nodes.pop_front();
+        cipher_mutation.extend(nodes);
+
+        let bounded_amount_to_shift = amount_to_shift % cipher_mutation.len() as i32;
+
+        let destination_index = if bounded_amount_to_shift < 0 && bounded_amount_to_shift.abs() as usize > index_of_node_needing_moved {
+            let remainder = (index_of_node_needing_moved as i32 + bounded_amount_to_shift).abs() as usize;
+            cipher_mutation.len() - remainder
         } else {
-            (bounded_amount_to_shift + index_of_node_needing_moved as i32).abs() as usize % (data.len())
+            (((index_of_node_needing_moved as i32) + bounded_amount_to_shift) % cipher_mutation.len() as i32) as usize
         };
 
-        if index_of_node_needing_moved == destination_index {
-            // do nothing
-            continue;
-        }
-
-        let is_going_right = amount_to_shift.is_positive();
-
-        let removal_index_offset;
-        if is_going_right {
-            // we're going to the RIGHT, so nodes we replace will be to the LEFT
-            if destination_index == 0 {
-                // what is current in front needs to be put back to the back
-                // because w're going right, it swaps with the final element
-                let front_node = cipher_mutation.pop_front().unwrap();
-                cipher_mutation.push_front((key, *amount_to_shift));
-                cipher_mutation.push_back(front_node);
-                removal_index_offset = 0; // zero because we swapped
-            } else if destination_index == cipher_mutation.len() - 1 {
-                // because we're going to the right, the final element just gets swapped
-                cipher_mutation.push_back((key, *amount_to_shift));
-                removal_index_offset = 0; // 0 because we're just pushing into the back
-            } else {
-                // we're going in the middle
-                // the node we're targetting needs to be BEHIND us
-                let mut destination_node_and_all_after = cipher_mutation.split_off(destination_index);
-                let front_node = destination_node_and_all_after.pop_front().unwrap();
-                destination_node_and_all_after.push_front((key, *amount_to_shift));
-                destination_node_and_all_after.push_front(front_node);
-                cipher_mutation.extend(destination_node_and_all_after);
-                if destination_index < index_of_node_needing_moved {
-                    // move the front node to the back
-                    // doesn't matter, but makes tests easier to reason about
-                    let front = cipher_mutation.pop_front().unwrap();
-                    cipher_mutation.push_back(front);
-                }
-
-                removal_index_offset = 0;
-            }
-        } else {
-            // we're going to the LEFT, so nodes we replace will be to the RIGHT of me
-            if destination_index == 0 {
-                // if we're approaching from the right, then this element needs to be moved to the back
-                // we can simply push back, removal index is incremented by one
-                cipher_mutation.push_front((key, *amount_to_shift));
-                removal_index_offset = 1;
-            } else if destination_index == cipher_mutation.len() - 1 {
-                // we're approaching this from the right
-                let back_element = cipher_mutation.pop_back().unwrap();
-                cipher_mutation.push_back((key, *amount_to_shift));
-                cipher_mutation.push_front(back_element);
-                removal_index_offset = 1;
-            } else {
-                // we're going in the middle
-                // the node we're targetting needs to be BEHIND us
-                let mut destination_node_and_all_after = cipher_mutation.split_off(destination_index);
-                destination_node_and_all_after.push_front((key, *amount_to_shift));
-                cipher_mutation.extend(destination_node_and_all_after);
-                if destination_index > index_of_node_needing_moved {
-                    // move the end to the beginning
-                    // it doesn't actually matter but it makes tests easier to reason about
-                    let back = cipher_mutation.pop_back().unwrap();
-                    cipher_mutation.push_front(back);
-                }
-
-                removal_index_offset = 1;
-            }
-        }
-
-        let index_needing_removed_after_shift = index_of_node_needing_moved + removal_index_offset % cipher_mutation.len();
-
-        // Remove the element from the buffer
-        if index_needing_removed_after_shift == 0 {
-            // no stitching. Remove the node from the front
-            // Then handle the destination
-            cipher_mutation.pop_front();
-        } else if index_needing_removed_after_shift == cipher_mutation.len() - 1 {
-            // we're at the end of the array
-            // no stitching, just pop off the element
-            cipher_mutation.pop_back();
-        } else {
-            // we're in the middle
-            // remove the element, stitch the arrays,
-            let mut current_node_and_all_after = cipher_mutation.split_off(index_needing_removed_after_shift);
-            current_node_and_all_after.pop_front();
-            cipher_mutation.extend(current_node_and_all_after);
-        }
-
-     //   println!("{:?}", cipher_mutation.iter().map(|(_key, shift)| shift).collect::<Vec<&i32>>());
+        // we now have our index that we're going to insert behind
+        let mut destination_node_and_all_after = cipher_mutation.split_off(destination_index);
+        destination_node_and_all_after.push_front((key, *amount_to_shift));
+        cipher_mutation.extend(destination_node_and_all_after);
     }
 
     let mixed_data = cipher_mutation.into_iter().map(|(_key, shift)| shift).collect::<Vec<i32>>();
@@ -173,8 +108,8 @@ mod tests {
         // 1b, 1a, 1c, 3 1a
         // 1a, 1b, 1c, 3 1b
         // 1a, 1b, 3, 1c 1c
-        // 1a, 3, 1b, 1c
-        assert_eq!(vec![1, 3, 1, 1], mix_cipher(vec![1, 1, 1, 3]));
+        // 1a, 1b, 3, 1c 3 SAME AS 1c, 1a, 1b, 3 SAME AS 3, 1c, 1a, 1b
+        assert_eq!(vec![3, 1, 1, 1], mix_cipher(vec![1, 1, 1, 3]));
     }
 
     #[test]
@@ -186,55 +121,73 @@ mod tests {
 
     #[test]
     fn wraps_left_correct() {
-        assert_eq!(vec![0, 0, 0, -3], mix_cipher(vec![0, 0, -3, 0]));
-        assert_eq!(vec![0, 0, -3, 0], mix_cipher(vec![0, -3, 0, 0]));
-        assert_eq!(vec![0, -3, 0, 0], mix_cipher(vec![-3, 0, 0, 0]));
-        assert_eq!(vec![-4, 0, 0, 0], mix_cipher(vec![-4, 0, 0, 0]));
-        assert_eq!(vec![0, -4, 0, 0], mix_cipher(vec![0, -4, 0, 0]));
-        assert_eq!(vec![0, 0, 0, -4], mix_cipher(vec![0, 0, 0, -4]));
-        assert_eq!(vec![0, 0, 0, -7], mix_cipher(vec![0, 0, -7, 0]));
-        assert_eq!(vec![0, 0, -7, 0], mix_cipher(vec![0, -7, 0, 0]));
-        assert_eq!(vec![0, -7, 0, 0], mix_cipher(vec![-7, 0, 0, 0]));
+        assert_eq!(vec![0, 0, -1, 0], mix_cipher(vec![0, 0, 0, -1]));
+        assert_eq!(vec![0, -2, 0, 0], mix_cipher(vec![0, 0, 0, -2]));
+        assert_eq!(vec![-3, 0, 0, 0], mix_cipher(vec![0, 0, 0, -3]));
+        assert_eq!(vec![0, 0, -4, 0], mix_cipher(vec![0, 0, 0, -4]));
+        assert_eq!(vec![0, 0, -3, 0], mix_cipher(vec![0, 0, -3, 0]));
+        assert_eq!(vec![0, -4, 0, 0], mix_cipher(vec![0, 0, -4, 0]));
+        assert_eq!(vec![-5, 0, 0, 0], mix_cipher(vec![0, 0, -5, 0]));
+        assert_eq!(vec![0, 0, -6, 0], mix_cipher(vec![0, 0, -6, 0]));
     }
 
     #[test]
     fn handles_wrapping_fine() {
-        assert_eq!(vec![0,0,4,0], mix_cipher(vec![0,0,4,0]));
-        assert_eq!(vec![0,0,8,0], mix_cipher(vec![0,0,8,0]));
-        assert_eq!(vec![0,0,12,0], mix_cipher(vec![0,0,12,0]));
+        assert_eq!(vec![0,0,3,0], mix_cipher(vec![0,0,3,0]));
+        assert_eq!(vec![0,0,6,0], mix_cipher(vec![0,0,6,0]));
+        assert_eq!(vec![0,0,9,0], mix_cipher(vec![0,0,9,0]));
     }
 
     #[test]
     fn wraps_right_correct() {
-        assert_eq!(vec![0,0,0,5], mix_cipher(vec![0,0,5,0]));
-        assert_eq!(vec![0,0,0,9], mix_cipher(vec![0,0,9,0]));
-        assert_eq!(vec![0,0,0,13], mix_cipher(vec![0,0,13,0]));
-        assert_eq!(vec![14,0,0,0], mix_cipher(vec![0,0,14,0]));
-        assert_eq!(vec![0,15,0,0], mix_cipher(vec![0,0,15,0]));
-        assert_eq!(vec![0,0,0,4], mix_cipher(vec![0,0,0,4]));
-        assert_eq!(vec![5,0,0,0], mix_cipher(vec![0,0,0,5]));
-        assert_eq!(vec![0,6,0,0], mix_cipher(vec![0,0,0,6]));
-        assert_eq!(vec![4,0,0,0], mix_cipher(vec![4,0,0,0]));
-        assert_eq!(vec![0,5,0,0], mix_cipher(vec![5,0,0,0]));
-        assert_eq!(vec![0,0,6,0], mix_cipher(vec![6,0,0,0]));
-        assert_eq!(vec![0,0,0,7], mix_cipher(vec![7,0,0,0]));
+        // A B C D 00
+        // B A C D 01
+        // B C A D 02
+        // B C D A 03 SAME AS A B C D
+        // B A C D 04
+        assert_eq!(vec![0,1,0,0], mix_cipher(vec![1,0,0,0]));
+        assert_eq!(vec![0,0,2,0], mix_cipher(vec![2,0,0,0]));
+        assert_eq!(vec![3,0,0,0], mix_cipher(vec![3,0,0,0]));
+        assert_eq!(vec![0,4,0,0], mix_cipher(vec![4,0,0,0]));
+        assert_eq!(vec![0,0,5,0], mix_cipher(vec![5,0,0,0]));
+        assert_eq!(vec![6,0,0,0], mix_cipher(vec![6,0,0,0]));
+        assert_eq!(vec![0,7,0,0], mix_cipher(vec![7,0,0,0]));
+        assert_eq!(vec![0,0,8,0], mix_cipher(vec![8,0,0,0]));
+        assert_eq!(vec![9,0,0,0], mix_cipher(vec![9,0,0,0]));
+
+        assert_eq!(vec![0,0,1,0], mix_cipher(vec![0,1,0,0]));
+        assert_eq!(vec![2,0,0,0], mix_cipher(vec![0,2,0,0]));
+        assert_eq!(vec![0,3,0,0], mix_cipher(vec![0,3,0,0]));
+        assert_eq!(vec![0,0,4,0], mix_cipher(vec![0,4,0,0]));
+        assert_eq!(vec![5,0,0,0], mix_cipher(vec![0,5,0,0]));
+        assert_eq!(vec![0,6,0,0], mix_cipher(vec![0,6,0,0]));
+        assert_eq!(vec![0,0,7,0], mix_cipher(vec![0,7,0,0]));
+        assert_eq!(vec![8,0,0,0], mix_cipher(vec![0,8,0,0]));
+        assert_eq!(vec![0,9,0,0], mix_cipher(vec![0,9,0,0]));
+
+        assert_eq!(vec![0,1,0,0], mix_cipher(vec![0,0,0,1]));
+        assert_eq!(vec![0,0,2,0], mix_cipher(vec![0,0,0,2]));
+        assert_eq!(vec![3,0,0,0], mix_cipher(vec![0,0,0,3]));
+        assert_eq!(vec![0,4,0,0], mix_cipher(vec![0,0,0,4]));
+        assert_eq!(vec![0,0,5,0], mix_cipher(vec![0,0,0,5]));
+        assert_eq!(vec![6,0,0,0], mix_cipher(vec![0,0,0,6]));
+        assert_eq!(vec![0,7,0,0], mix_cipher(vec![0,0,0,7]));
+        assert_eq!(vec![0,0,8,0], mix_cipher(vec![0,0,0,8]));
+        assert_eq!(vec![9,0,0,0], mix_cipher(vec![0,0,0,9]));
     }
 
     #[test]
     fn test_input_wraps_correctly () {
-        assert_eq!(vec![1, 2, -3, 4, 0, 3, -2], mix_cipher(vec![1,2,-3,3,-2,0,4]));
-    }
+        // 1, 2, -3, 3, -2, 0, 4
+        // 2, 1, -3, 3, -2, 0, 4
+        // 1, -3, 2, 3, -2, 0, 4
+        // 1, 2, 3, -2, -3, 0, 4
+        // 1, 2, -2, -3, 0, 3, 4
+        // 1, 2, -3, 0, 3, 4, -2
+        // 1, 2, -3, 0, 3, 4, -2
+        // 1, 2, -3, 4, 0, 3, -2
 
-    #[test]
-    fn wraps() {
-        assert_eq!(vec![1, 2, -3, 4, 7, 3, -2], mix_cipher(vec![1,2,-3,3,-2,7,4]));
-        assert_eq!(vec![1, 2, -3, 4, -7, 3, -2], mix_cipher(vec![1,2,-3,3,-2,-7,4]));
-        assert_eq!(vec![1, 2, -3, 4, -14, 3, -2], mix_cipher(vec![1,2,-3,3,-2,-14,4]));
-        assert_eq!(vec![1], mix_cipher(vec![1]));
-        assert_eq!(vec![2], mix_cipher(vec![2]));
-        assert_eq!(vec![2, 1], mix_cipher(vec![1, 2]));
-        assert_eq!(vec![2, -1], mix_cipher(vec![-1, 2]));
-        assert_eq!(vec![-2, 1], mix_cipher(vec![1, -2]));
+        assert_eq!(vec![1, 2, -3, 4, 0, 3, -2], mix_cipher(vec![1,2,-3,3,-2,0,4]));
     }
 
     #[test]
@@ -252,7 +205,14 @@ mod tests {
 
     #[test]
     fn second() {
-        let input = "";
-        assert_eq!(0, problem_2(&input));
+        let input = "\
+1
+2
+-3
+3
+-2
+0
+4";
+        assert_eq!(1623178306, problem_2(&input));
     }
 }
